870e86b52a8668432cfe097e6775b1d0540e7e60
Marko Kosunen <marko.kosunen@aalto.fi>
2023-03-05 13:17:38 +0200

    Submodule parsing cleaned up
    Potlayuot flag no longer extracted. Extraction vas vaguely defined
    and unrelated to module handling.

--- spice/spice_module.py
+++ spice/spice_module.py
@@ -55,11 +55,12 @@ class spice_module(thesdk):
         """Boolean
         
         Flag for detected post-layout netlists. This will enable post-layout
-        optimizations in the simulator command options. Automatically detected
-        from given netlist for Calibre extracted netlists, or when 'dspf'
-        attribute is defined.
+        optimizations in the simulator command options. 
+
         """
         if not hasattr(self,'_postlayout'):
+            self.print_log(type='F', 
+                           msg='Postlayout attribute accessed before defined. This parameter is no longer automatically extracted. You must set it.')
             self._postlayout=False
         return self._postlayout
     @postlayout.setter
@@ -76,130 +77,15 @@ class spice_module(thesdk):
         String containing the contents of the subcircuit definition of the entity.
         Extract the definition form the source netlist. the source netlist when accessed. 
         Can be written to the subckt_file with export_subckt method. 
+
         """
         if not hasattr(self,'_subckt'):
-            startmatch=re.compile(r"%s" %(self.parent.syntaxdict["subckt"]),re.IGNORECASE)
-            endmatch=re.compile(r"%s" %(self.parent.syntaxdict["subend"]),re.IGNORECASE)
-            
-            # TODO This should be improved. the topcell name is known already, and it matches the 
-            # subcircuit name. there is no reason to rely on some strings in some file that
-            # depends on what genertes the file
-            # These are used for post-layout only
-            # Works only for Calibre generated postlayout netlists.
-            if self.parent.model=='eldo':
-                cellnamematch=re.compile(r"\*\*\* Design cell name:",re.IGNORECASE)
-                prognamematch=re.compile(r"\* Program",re.IGNORECASE)
-            elif self.parent.model=='spectre':
-                cellnamematch=re.compile(r"\/\/ Design cell name:",re.IGNORECASE)
-                prognamematch=re.compile(r"\/\/ Program",re.IGNORECASE)
-            elif self.parent.model=='ngspice':
-                cellnamematch=re.compile(r"\*\*\* Design cell name:",re.IGNORECASE)
-                prognamematch=re.compile(r"\* Program",re.IGNORECASE)
-
-            cellname = ''
-            linecount = 0
             self._subckt="%s Subcircuit definitions\n\n" % self.parent.syntaxdict["commentchar"]
             # Extract the module definition
             if os.path.isfile(self._dutfile):
                 try:
                     self.print_log(type='D',msg='Parsing source netlist %s' % self._dutfile)
                     self._subckt += subprocess.check_output('sed -n \'/\.*[sS][uU][bB][cC][kK][tT]/,/\.*[eE][nD][sS]/p\' %s' % self._dutfile, shell=True).decode('utf-8')
-                    #with open(self._dutfile) as infile:
-                    #    wholefile=infile.readlines()
-                    #    startfound=False
-                    #    endfound=False
-                    #    for line in wholefile:
-                    #        # First subcircuit not started, finding ADEL written cell name
-                    #        if not startfound and cellnamematch.search(line) != None:
-                    #            words = line.split()
-                    #            cellname = words[-1]
-                    #            self.print_log(type='D',msg='Found top-level cell name "%s".' % cellname)
-                    #            self.origcellname = cellname
-                    #        # First subcircuit not started, finding Calibre xRC written program name
-                    #        # What is so special about the calibre generated subcirsuit definition?
-                    #        if not startfound and prognamematch.search(line) != None:
-                    #            self.print_log(type='D',msg='Post-layout netlist detected (%s).' % (' '.join(line.split()[2:])))
-                    #            self.postlayout = True
-                    #            # THIS IS THE PROBLEM PART
-                    #            # In pre layout, the file is NOT copied as it may contain something else
-                    #            # Than the subckt definitions (i.e simulator directives)
-                    #            # In post layout it MUST be copied because of what? Slow parsing?
-                    #            # I'll leave it for now. In any case, 'subinst' should not parse the file again. 
-                    #            # Parsing the post-layout netlist line by line is way too slow
-                    #            # Copying the file and editing it seems better
-
-                    #            # Right now this just ignores everything and overwrites the subcircuit file
-                    #            # TODO: think about this
-                    #            if os.path.isfile(self._subcktfile):
-                    #                os.remove(self._subcktfile)
-                    #                shutil.copyfile(self._dutfile,self._subcktfile)
-                    #            else:
-                    #                shutil.copyfile(self._dutfile,self._subcktfile)
-                    #            # Edits the postlayout netlist file in place
-                    #            for line in fileinput.input(self._subcktfile,inplace=1):
-                    #                startfound=False
-                    #                endfound=False
-                    #                if not startfound and startmatch.search(line) != None:
-                    #                    startfound=True
-                    #                    words = line.split()
-                    #                    # There has been no match for cellname.
-                    #                    if cellname == '':
-                    #                        cellname = words[1].lower()
-                    #                    # If the cellname has been set above, it is set to slef.parent.name
-                    #                    # Are they different for some reason?
-                    #                    if words[1].lower() == cellname.lower():
-                    #                        words[1] = self.parent.name
-                    #                        line = ' '.join(words) + "\n"
-                    #                # self._subckt contains the subcircuit definition.
-                    #                self._subckt += line 
-                    #                sys.stdout.write(line)
-                    #            #Why would this happen?
-                    #            if cellname != self.parent.name:
-                    #                self.print_log(type='D',msg='Renaming design cell %s to %s.' % (cellname,self.parent.name))
-                    #            # Notice the return here
-                    #            return self._subckt
-                    #        # First subcircuit not started, starts on this line though
-                    #        if not startfound and startmatch.search(line) != None:
-                    #            startfound=True
-                    #            words = line.split()
-                    #            # Either it's a postlayout netlist, or the cell name was not defined
-                    #            # in the header -> assuming first subcircuit is top-level circuit
-                    #            # This is confusing. Why we need to make this assumption. The top-level
-                    #            # Circuit IS the name of the entity.
-                    #            if cellname == '':
-                    #                cellname = words[1].lower()
-                    #                self.print_log(type='D',msg='Renaming design cell %s to %s.' % (cellname,self.parent.name))
-                    #            if words[1].lower() == cellname.lower():
-                    #                self._subckt+="\n%s Subcircuit definition for %s module\n" % (self.parent.syntaxdict["commentchar"],self.parent.name)
-                    #                words[1] = self.parent.name
-                    #                if cellname != self.parent.name:
-                    #                    self.print_log(type='D',msg='Renaming design cell "%s" to "%s".' % (cellname,self.parent.name))
-                    #                line = ' '.join(words) + "\n"
-                    #                linecount += 1
-                    #        # Inside the subcircuit clause -> copy all lines except comments
-                    #        if startfound:
-                    #            words = line.split()
-                    #            if len(words) > 0 and words[0] != self.parent.syntaxdict["commentchar"]:
-                    #                if words[0] == 'subckts': #todo: figure out what this spectre line does
-                    #                    startfound=False
-                    #                else:
-                    #                    # Top-level subcircuit ends here, renaming old name to entity name
-                    #                    if len(words) > 0 and words[0].lower() == 'ends' \
-                    #                            and words[1].lower() == cellname.lower():
-                    #                        words[-1] = self.parent.name
-                    #                        line =  ' '.join(words) + '\n'
-                    #                    self._subckt=self._subckt+line
-                    #                    linecount += 1
-                    #        # Calibre places an include statement above the first subcircuit -> grab that
-                    #        if len(self.parent.dspf) == 0 and self.postlayout and not startfound:
-                    #            words = line.split()
-                    #            if words[0].lower() == self.parent.syntaxdict["include"]:
-                    #                self._subckt=self._subckt+line
-                    #                linecount += 1
-                    #        # End of subcircuit found
-                    #        if startfound and endmatch.search(line) != None:
-                    #            startfound=False
-                    #self.print_log(type='D',msg='Source netlist parsing done (%d lines).' % linecount)
                 except:
                     self.print_log(type='E',msg='Something went wrong while parsing %s.' % self._dutfile)
                     self.print_log(type='E',msg=traceback.format_exc())
@@ -223,13 +109,7 @@ class spice_module(thesdk):
         try:
             if not hasattr(self,'_subinst'):
                 subckt = self.subckt.split('\n')
-                #if self.postlayout:
-                #    #This means that _subcktfile has already been written
-                #    # However, the self._subckt has been parsed from there already. 
-                #    with open(self._subcktfile) as infile:
-                #      # Here we parse the copied subcircuit file again? Makes no sense at all. 
-                #      subckt=infile.readlines()
-                startmatch=re.compile(r"%s %s " %(self.parent.syntaxdict["subckt"], self.parent.name.upper())
+                startmatch=re.compile(r"%s %s " %(self.parent.syntaxdict["subckt"], self.parent.name)
                         ,re.IGNORECASE)
 
                 if len(subckt) <= 3:
@@ -242,24 +122,24 @@ class spice_module(thesdk):
                     endfound = False
                     lastline = False
                     for line in subckt:
-                        #if self.postlayout:
-                        #    if self.parent.model == 'spectre':
-                        #        # Does this actually doe something?
-                        #        # Replaces newlines on line with space
-                        #        line = line.replace('\n','')
+                        if self.postlayout:
+                            if self.parent.model == 'spectre':
+                                # Does this actually doe something?
+                                # Replaces newlines on _line_ with space
+                                line = line.replace('\n','')
 
                         if startmatch.search(line) != None:
                             startfound = True
                             #For spectre, for some reason we need to process this already on the first line
-                            if self.parent.model == 'spectre':
-                                if startfound and len(line) > 0:
-                                    if lastline:
-                                        endfound = True
-                                        startfound = False
-                                    # If the last character of the line is not backslash
-                                    # escaping newline we are already at the end
-                                    if not line[-1] == '\\':
-                                        lastline = True
+                            #if self.parent.model == 'spectre':
+                            #    if startfound and len(line) > 0:
+                            #        if lastline:
+                            #            endfound = True
+                            #            startfound = False
+                            #        # If the last character of the line is not backslash
+                            #        # escaping newline we are already at the end
+                            #        if not line[-1] == '\\':
+                            #            lastline = True
                         elif startfound and len(line) > 0:
                             if self.parent.model == 'eldo':
                                 if line[0] != '+':
