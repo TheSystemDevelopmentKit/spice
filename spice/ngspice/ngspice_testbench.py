905867d0ae8f0b56c85d1c05e09a14400f5385cc
Miikka Mäki <miikka.maki@aalto.fi>
2026-06-09 09:30:22 +0300

    oops wrong way around

--- spice/ngspice/ngspice_testbench.py
+++ spice/ngspice/ngspice_testbench.py
@@ -381,10 +381,10 @@ class ngspice_testbench(testbench_common):
                             msg="Inferred transient duration is %g s from '%s'."
                             % (simtime, self._trantime_name),
                         )
-                    tstop = val.tprint if val.tprint else val.strobeperiod
+                    tstep = val.strobeperiod if val.strobeperiod else val.tprint
                     self._simcmdstr += ".%s %s %s %s\n" % (
                         sim,
-                        str(tstop),
+                        str(tstep),
                         str(simtime),
                         "uic" if val.uic else "",
                     )
